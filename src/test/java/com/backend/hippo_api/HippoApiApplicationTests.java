94b26feb19fb4804650ea501da6163ab82962dcb
Vinicius <viniciusbonzaki@gmail.com>
2026-08-17 19:13:21 -0300

    Commit Inicial

---
+++ src/test/java/com/backend/hippo_api/HippoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.hippo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
